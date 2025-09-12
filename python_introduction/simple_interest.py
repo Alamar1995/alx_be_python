224e8bbf10e1c77409f2ef4f102817687db8e53c
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2025-09-12 09:03:25 +0300

    Add code to calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+# python_introduction/simple_interest.py
+
+# Assign values
+principal = 1000
+rate = 5       # interest rate in percent
+time = 2       # time in years
+
+# Calculate simple interest
+simple_interest = (principal * rate * time) / 100
+
+# Print the result
+print("The simple interest is:", simple_interest)
