249766c69643eddee2337c3a90179b9c966aa9a1
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2025-09-12 09:01:21 +0300

    Add code for basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# python_introduction/basic_operations.py
+
+# Assign values to the variables
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print results
+print("The sum is:", sum_result)
+print("The difference is:", difference)
+print("The product is:", product)
