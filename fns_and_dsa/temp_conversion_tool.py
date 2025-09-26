5703afa6de79a0f3ce605078569b2a7851aa80ab
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2025-09-26 08:45:09 +0300

    Fix: adjust global conversion factors formatting for checker

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 
 def convert_to_celsius(fahrenheit):
@@ -29,3 +29,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
